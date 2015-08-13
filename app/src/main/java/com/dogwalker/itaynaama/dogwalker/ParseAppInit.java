55b869760f6a97e6150667e130b9a7636bb983e7
Itay <imalul@hotmail.com>
2015-08-14 02:47:47 +0300

    login is working now. added a few notes in code. User class deleted. new class for initialize parse - ParseAppInit

---
+++ app/src/main/java/com/dogwalker/itaynaama/dogwalker/ParseAppInit.java
@@ -0,0 +1,17 @@
+package com.dogwalker.itaynaama.dogwalker;
+
+import android.app.Application;
+import com.parse.Parse;
+
+
+public class ParseAppInit extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+
+        //initialize parse for using data control.
+        Parse.initialize(this,getString(R.string.ApplicationID),getString(R.string.ClientKey));
+    }
+}
--- app/src/main/java/com/dogwalker/itaynaama/dogwalker/SearchActivity.java
+++ app/src/main/java/com/dogwalker/itaynaama/dogwalker/SearchActivity.java
@@ -3,11 +3,11 @@ package com.dogwalker.itaynaama.dogwalker;
 import android.location.Location;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
@@ -15,7 +15,7 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 
-public class SearchActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener
+public class SearchActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener
 {
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
