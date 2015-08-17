2cda70f6c8de70a53bca1e1ec1fd10a5b1cbfb45
Itay <imalul@hotmail.com>
2015-08-17 23:49:25 +0300

    forgot password button added to login screen fully functional

--- app/src/main/java/com/dogwalker/itaynaama/dogwalker/Login.java
+++ app/src/main/java/com/dogwalker/itaynaama/dogwalker/Login.java
@@ -16,12 +16,14 @@ import android.widget.Toast;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.RequestPasswordResetCallback;
 
 
 public class Login extends AppCompatActivity implements View.OnClickListener
 {
 
     Button loginB;
+    Button forgotPassB;
     EditText usernameET;
     EditText passwordET;
     SharedPreferences userPref;
@@ -34,12 +36,15 @@ public class Login extends AppCompatActivity implements View.OnClickListener
 
         //reference the login button from the activity to this button.
         loginB = (Button) findViewById(R.id.loginButton);
+        //reference the forgot password button from the activity to this button.
+        forgotPassB = (Button) findViewById(R.id.forgotPasswordButton);
         //reference the user button from the activity to this button.
         usernameET = (EditText)findViewById(R.id.usernameEditText);
         //reference the login button from the activity to this button.
         passwordET = (EditText)findViewById(R.id.passwordEditText);
 
         loginB.setOnClickListener(this);
+        forgotPassB.setOnClickListener(this);
     }
 
     @Override
@@ -70,6 +75,46 @@ public class Login extends AppCompatActivity implements View.OnClickListener
                 });
 
                 break;
+
+            case R.id.forgotPasswordButton:
+                //perform user password reset by sending a password reset request using provided email address.
+                final android.support.v7.app.AlertDialog.Builder alertBuilder = new android.support.v7.app.AlertDialog.Builder(this);
+                final EditText input = new EditText(this);
+//                input.setId(0);
+
+                alertBuilder.setMessage("Do you want to reset your password \n(sending reset to your mail)")
+                        .setPositiveButton("Send reset", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                //send the reset to the mail, if not successful print exception.
+                                ParseUser.requestPasswordResetInBackground(input.getText().toString(), new RequestPasswordResetCallback()
+                                {
+                                    @Override
+                                    public void done(ParseException e)
+                                    {
+                                        if (e == null)
+                                        {
+                                            Toast.makeText(getApplicationContext(), "reset sent successfully to your email", Toast.LENGTH_LONG).show();
+                                        }
+                                        else
+                                        {
+                                            Log.d("My Loggggg", e.getMessage());
+                                            Toast.makeText(getApplicationContext(), "error" + e.getMessage(), Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                });
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.d("My Loggggg", "user canceled reset");
+                            }
+                        });
+                alertBuilder.setView(input);
+                alertBuilder.show();
         }
     }
 
